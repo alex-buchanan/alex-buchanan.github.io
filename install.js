8e24b63587cad7080ffc01e3c4dbb41d97680842
alex-buchanan <alex.buchanan210@gmail.com>
2020-08-18 02:10:25 -0400

    install prompt

--- install.js
+++ install.js
@@ -1,27 +1,8 @@
-// CODELAB: Add event listener for beforeinstallprompt event
-/* window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
-
 function saveBeforeInstallPromptEvent(evt) {
   // CODELAB: Add code to save event & show the install button.
+  window.removeEventListener("beforeinstallprompt", saveBeforeInstallPromptEvent);
+}
 
-} */
-
-// CODELAB: Add code to save event & show the install button.
-deferredInstallPrompt = evt;
-installButton.removeAttribute('hidden');
-
-// CODELAB: Add code show install prompt & hide the install button.
-deferredInstallPrompt.prompt();
-// Hide the install button, it can't be called twice.
-evt.srcElement.setAttribute('hidden', true);
-
-// CODELAB: Log user response to prompt.
-deferredInstallPrompt.userChoice.then((choice) => {
-      if (choice.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt', choice);
-      } else {
-        console.log('User dismissed the A2HS prompt', choice);
-      }
-      deferredInstallPrompt = null;
-    });
+// CODELAB: Add event listener for beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
 
