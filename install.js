6c5fc27ec7c3cc731f4532ef7aa0fb1d6fc37706
alex-buchanan <alex.buchanan210@gmail.com>
2020-08-19 14:20:04 -0400

    added install button, fixed functionality, removes button when installed

--- install.js
+++ install.js
@@ -1,19 +1,38 @@
 // CODELAB: Add event listener for beforeinstallprompt event
 let deferredPrompt;
 
+function addToHome()
+{
+	document.getElementById('cover').style.opacity = "0.5";
+	document.getElementById('cover').style.zIndex = "3";
+	deferredPrompt.prompt();
+	deferredPrompt.userChoice.then((choiceResult) => {
+		if (choiceResult.outcome === 'accepted') {
+		  	console.log('User accepted the install prompt');
+		  	document.getElementById('cover').style.opacity = "0";
+			document.getElementById('cover').style.zIndex = "-1";
+			var addbtn = document.getElementById("buttonInstall");
+			addbtn.parentNode.removeChild(addbtn);
+
+		} else {
+		  	console.log('User dismissed the install prompt');
+		  	document.getElementById('cover').style.opacity = "0";
+			document.getElementById('cover').style.zIndex = "-1";
+		}
+	});
+}
+
+function showInstallPromotion()
+{
+	document.getElementById("buttonInstall").addEventListener('click', addToHome );
+}
+
 window.addEventListener('beforeinstallprompt', (e) => {
 	e.preventDefault();
 	deferredPrompt = e;
 	showInstallPromotion();
 });
 
-function showInstallPromotion()
-{
-	document.getElementById("logo").addEventListener('click', () => {
-		document.getElementById('cover').style.opacity = "0.5";
-		document.getElementById('cover').style.zIndex = "3";
-		deferredPrompt.prompt();
-	});
-}
+
 
 
