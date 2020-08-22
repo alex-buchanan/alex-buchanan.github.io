d51ba8872a41669811ef5fbbb628dda69e8771aa
alex-buchanan <alex.buchanan210@gmail.com>
2020-08-21 23:02:59 -0400

    Added functionality that doesn't show install button in app view

--- install.js
+++ install.js
@@ -33,6 +33,26 @@ window.addEventListener('beforeinstallprompt', (e) => {
 	showInstallPromotion();
 });
 
+window.addEventListener('DOMContentLoaded', () => {
+  let displayMode = 'browser tab';
+  if (navigator.standalone) {
+    displayMode = 'standalone-ios';
+    document.getElementById('cover').style.opacity = "0";
+	document.getElementById('cover').style.zIndex = "-1";
+	var addbtn = document.getElementById("buttonInstall");
+	addbtn.parentNode.removeChild(addbtn);
+  }
+  if (window.matchMedia('(display-mode: standalone)').matches) {
+    displayMode = 'standalone';
+    document.getElementById('cover').style.opacity = "0";
+	document.getElementById('cover').style.zIndex = "-1";
+	var addbtn = document.getElementById("buttonInstall");
+	addbtn.parentNode.removeChild(addbtn);
+  }
+  // Log launch display mode to analytics
+  console.log('DISPLAY_MODE_LAUNCH:', displayMode);
+});
+
 
 
 
